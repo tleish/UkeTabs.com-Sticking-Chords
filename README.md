bec8f3f4c672730f60a12e9882b695d7efd43dc8
tleish <tleish@hotmail.com>
2013-12-27 09:12:53 -0700

    Initital commit of UkeTabs.com-Sticky-Chords

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-UkeTabs.com-Sticking-Chords
+UkeTabs.com Sticking Chords
 ===========================
 
 A Google Chrome extension specifically for UkeTabs.com which causes chord charts to hover at the top of the browser window while scrolling the page.
+
+# ![image](https://raw.github.com/tleish/uketabs/master/icons/icon48.png) UkeTabs.com Sticking Chords
+
+## Installation
+
+1. Download and install from https://chrome.google.com/webstore/.
+
+## License
+
+You may use, modify and distribute UkeTabs.com Sticky Chords freely under the MIT license. See the LICENCE file contained in this project for more details.
---
+++ inject/inject.js
@@ -0,0 +1,66 @@
+//jQuery(".floating-chords").remove();
+//$(window).unbind('scroll');
+
+(function( $ ) {
+
+  var uketabs = (function () {
+    var $area_for_scroll;
+    var $chord_div;
+    var $floating_chord_div;
+//    var is_floating = false;
+
+
+    var init = function(){
+      $area_for_scroll = $("#area_for_scroll");
+      $chord_div = $area_for_scroll.find(".chord").parent();
+      create_floating_div();
+      show_hide_div();
+    }
+
+    var create_floating_div = function(){
+      $floating_chord_div =  $chord_div.clone();
+      $floating_chord_div.addClass("sticky-chords");
+      $area_for_scroll.append($floating_chord_div);
+    }
+
+    var chord_div_offset_top = function(){
+      return $chord_div.offset()["top"];
+    }
+
+    var content_bottom = function(){
+      var $cont = $("#cont");
+      var top = $cont.offset()["top"];
+      var height = $cont.height();
+      return top + height;
+    }
+
+    var show_hide_div = function(){
+      var top = chord_div_offset_top();
+      var bottom = content_bottom() - $floating_chord_div.height();
+      $floating_chord_div.hide();
+      window_scroll_binding(top, bottom);
+    }
+
+    var window_scroll_binding = function(top, bottom){
+      $(window).bind('scroll', function() {
+        var scroll_top = $(window).scrollTop();
+        if (scroll_top > top && scroll_top < bottom) {
+          $floating_chord_div.show();
+        } else {
+          $floating_chord_div.hide();
+        }
+      });
+    }
+
+    return {
+      init: init
+    };
+  })();
+
+  console.log("uke tabs init");
+  uketabs.init();
+
+})( jQuery );
+
+
+console.log("uke tabs loaded");
